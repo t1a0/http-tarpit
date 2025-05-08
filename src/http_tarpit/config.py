ae448d1f98f33578ad298ad15204734f50bc5332
t1a <spesivtsevwork@gmail.com>
2025-05-08 15:52:40 +0300

    feat: Add 19 = Bad Web Bot categories to config and increase hold time

--- src/http_tarpit/config.py
+++ src/http_tarpit/config.py
@@ -11,8 +11,8 @@ HOST = "127.0.0.1"
 PORT = 8080        
 LOG_DIR = BASE_DIR / "logs" 
 LOG_FILE = LOG_DIR / "tarpit.log" 
-LOG_LEVEL = logging.DEBUG 
-CONSOLE_LOG_LEVEL = logging.INFO 
+LOG_LEVEL = logging.INFO 
+CONSOLE_LOG_LEVEL = logging.WARNING 
 
 # bd config
 DATABASE_DIR = BASE_DIR / "data"
@@ -21,13 +21,13 @@ SQLITE_DB_FILE = DATABASE_DIR / "tarpit_events.db"
 # tarpit config
 RESPONSE_DELAY_SECONDS = 1.5
 RESPONSE_CHUNK = b'.'
-MAX_RESPONSE_BYTES = 50
+MAX_RESPONSE_BYTES = 1200
 
 ABUSEIPDB_API_KEY = os.getenv("ABUSEIPDB_API_KEY",None)
 ABUSEIPDB_ENABLED = bool(ABUSEIPDB_API_KEY)
 ABUSEIPDB_CONFIDENCE_SCORE = 90
 ABUSEIPDB_COMMENT_PREFIX = "HTTP Tarpit detected bot activity:"
-ABUSEIPDB_CATEGORIES = "14,21" # 14 = Port Scan, 21 = Web App Atack (?18 = Brute?)
+ABUSEIPDB_CATEGORIES = "14,21,19" # 14 = Port Scan, 21 = Web App Atack, 19 = Bad Web Bot (?18 = Brute?)
 
 GEOLITE2_CITY_DB_PATH = BASE_DIR / "data" / "GeoLite2-City.mmdb"
 GEOLITE2_ASN_DB_PATH = BASE_DIR / "data" / "GeoLite2-ASN.mmdb"
